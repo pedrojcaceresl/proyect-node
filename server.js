e03a10653fb91e0c6bc8e2f152433f892b69f64e
pedro <pedro.jcaceresl@gmail.com>
2023-02-25 11:51:05 -0300

    port changed

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('<h1>Express with HTML</h1>');
 })
 
-app.listen(3000); 
-console.log(`Server running on port 3000`);
+app.listen(8080); 
+console.log(`Server running on port 8080`);
