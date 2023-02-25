8eeb109d82c4d1ceab86b75fac487d97d37211a7
pedro <pedro.jcaceresl@gmail.com>
2023-02-25 09:31:23 -0300

    express installed

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    res.end('Hello world');
-});
+const app = express();
 
-server.listen(3000); 
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+app.listen(3000); 
 console.log(`Server running on port 3000`);
